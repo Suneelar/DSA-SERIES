6bbdeb447a68f70d7e3d396c067b1b842557a6f6
Suneelar <suneelar397@gmail.com>
2025-11-13 23:07:18 +0530

    Initial commit of print duplicates_printUnquie and Ferquency Java program

---
+++ PrintDuplicates.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class PrintDuplicates {
+
+    public static boolean containsDuplicates(String[] arr) {
+    Set<String> set = new HashSet<>();
+    for (String s : arr) {
+        if (!set.add(s)) {
+            return true; // Duplicate found
+        }
+    }
+    return false; // No duplicates
+}
+ public static void main(String[] args) {
+    String str="hi hi this this is java";
+    String s[]=str.split(" ");
+   Map<String,Integer>map=new LinkedHashMap<String,Integer>();
+for(String s2:s){
+    map.put(s2, map.getOrDefault(s2, 0)+1 );
+}
+map.forEach((key,value)->{
+//     if(value>1){
+//        System.out.println("This are the duplicates values:\t"+key+" "+value); 
+//     }else if(value==1){
+// System.out.println("This are the unquie values:\t"+key+" "+value); 
+//     }
+       System.out.println("This are the Fequency values:\t"+value);  
+    
+   
+});
+   
+ }   
+}
